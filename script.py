c74f22ea4e6fb9bcdfeea9ab6b069bb633bb9b52
Gabriel Mello <mellogcg@gmail.com>
2020-09-06 00:40:47 -0300

    Revert "Revert "Script rodando Pandas Profiling para conhecer melhor a base de dados""
    This reverts commit 365be22e9739c4aa9e4bf9ce9907f1be35b4f21a.

--- script.py
+++ script.py
@@ -13,14 +13,25 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 import os
+from pandas_profiling import ProfileReport
 
-os.getcwd()
-os.chdir('')
+# %%
+os.chdir('C:/Users/mello/OneDrive/Documentos/10 - Projetos/Analise_Vendas_VideoGame_Kaggle/Base')
 
 # %%
 # Carregando o dataset
-dados = pd.read_csv('Base/vgsales.csv')
+dados = pd.read_csv('vgsales.csv')
+# %%
+reporte=ProfileReport(dados, title='Pandas Profiling', explorative=True)
 
+#%%
+reporte.to_file("output/your_report.html")
+
+#%%
+reporte.to_widgets()
+
+#%%
+reporte.to_notebook_iframe()
 
 # %%
 dados.head(10)
